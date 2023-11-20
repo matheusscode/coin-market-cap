23949ca3dcb8343af3cb7684975e61aa2bb27ecb
Matheus Santos <99546472+matheusscode@users.noreply.github.com>
2023-11-20 05:42:11 +0000

    refactor: refactoring function of saving currency in localStorage and deleting

--- src/context/FavoriteContext/FavoriteProvider.tsx
+++ src/context/FavoriteContext/FavoriteProvider.tsx
@@ -1,18 +1,47 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useMemo } from "react";
 import { FavoriteContext } from "./FavoriteCreate";
+import { useToast } from "@chakra-ui/react";
+
+const TOAST_COLORS = {
+  info: "red",
+  success: "green",
+};
 
 interface FavoriteProviderProps {
   children: ReactNode;
 }
 
 export const FavoriteProvider = ({ children }: FavoriteProviderProps) => {
-  const [favoriteCoin, setFavoriteCoin] = useState<string[]>([]);
+  const [favoriteCoin, setFavoriteCoin] = useState<string[]>(() => {
+    const storedFavorites = localStorage.getItem("favoriteCoins");
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+  const toast = useToast();
 
   useEffect(() => {
-    setFavoriteCoin(
-      JSON.parse(localStorage.getItem("favoriteCoins") as string) || []
-    );
+    const storedFavorites = localStorage.getItem("favoriteCoins");
+    if (storedFavorites) {
+      setFavoriteCoin(JSON.parse(storedFavorites));
+    }
   }, []);
+
+  const showToast = (
+    action: string,
+    status: "info" | "success",
+    color: string
+  ) => {
+    toast({
+      title: action,
+      status,
+      duration: 2000,
+      isClosable: true,
+      position: "top-right",
+      containerStyle: {
+        background: color,
+        borderRadius: "10px",
+      },
+    });
+  };
 
   const toggleFavoriteCoin = (name: string) => {
     const isFavorite = favoriteCoin.includes(name);
@@ -26,17 +55,28 @@ export const FavoriteProvider = ({ children }: FavoriteProviderProps) => {
 
     localStorage.setItem("favoriteCoins", JSON.stringify(updatedFavorites));
     setFavoriteCoin(updatedFavorites);
+
+    const action = isFavorite
+      ? "Moeda removida dos favoritos com sucesso"
+      : "Moeda adicionada aos favoritos com sucesso";
+    const status: "info" | "success" = isFavorite ? "info" : "success";
+    const color = TOAST_COLORS[status];
+
+    showToast(action, status, color);
   };
 
   const isCoinSavedAsFavorite = (coinName: string): boolean =>
     favoriteCoin.includes(coinName);
 
-  const contextValue = {
-    favoriteCoin,
-    setFavoriteCoin,
-    toggleFavoriteCoin,
-    isCoinSavedAsFavorite,
-  };
+  const contextValue = useMemo(
+    () => ({
+      favoriteCoin,
+      setFavoriteCoin,
+      toggleFavoriteCoin,
+      isCoinSavedAsFavorite,
+    }),
+    [favoriteCoin, toggleFavoriteCoin, isCoinSavedAsFavorite]
+  );
 
   return (
     <FavoriteContext.Provider value={contextValue}>
