0310c2113b13bb1f77882ce626bacbe48177eb56
Matheus Santos <99546472+matheusscode@users.noreply.github.com>
2023-11-20 05:36:58 +0000

    faet: changing typing name to CoinProps in CardGit component

--- src/components/CardGIt.tsx
+++ src/components/CardGIt.tsx
@@ -1,51 +1,45 @@
-import React from 'react'
-import { Card, Heading,Skeleton,Text, useMediaQuery } from "@chakra-ui/react";
+import React from "react";
+import { Card, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 
 interface CardGitProps {
   title: string;
   value?: number;
-  loaded: boolean
+  loaded: boolean;
 }
 
 const CardGit: React.FC<CardGitProps> = ({ title, value, loaded }) => {
   const [isLargerThan700] = useMediaQuery("(max-width: 700px)");
 
   return (
-    <Skeleton
+    <Card
       w={isLargerThan700 ? "100%" : "319px"}
-      startColor="gray"
-      endColor="white"
-      isLoaded={loaded === false ? true : false}
+      h="119px"
+      borderRadius="8px"
+      bg="light"
+      border="1px solid #E5E5E5"
+      p="1.5rem"
     >
-      <Card
-        h="119px"
-        borderRadius="8px"
-        bg="light"
-        border="1px solid #E5E5E5"
-        p="1.5rem"
+      <Heading
+        as="h1"
+        lineHeight="1.21rem"
+        fontSize="1rem"
+        textAlign={isLargerThan700 ? "center" : "left"}
+        color="gray_slightly"
+        fontWeight={600}
       >
-        <Heading
-          as="h1"
-          lineHeight="1.21rem"
-          fontSize="1rem"
-          textAlign={isLargerThan700 ? "center" : "left"}
-          color="gray_slightly"
-          fontWeight={600}
-        >
-          {title}
-        </Heading>
-        <Text
-          color="deep_gray"
-          fontWeight={700}
-          textAlign={isLargerThan700 ? "center" : "left"}
-          lineHeight="3.025625rem"
-          fontSize="2.5rem"
-        >
-          {value}
-        </Text>
-      </Card>
-    </Skeleton>
+        {title}
+      </Heading>
+      <Text
+        color="deep_gray"
+        fontWeight={700}
+        textAlign={isLargerThan700 ? "center" : "left"}
+        lineHeight="3.025625rem"
+        fontSize="2.5rem"
+      >
+        {value}
+      </Text>
+    </Card>
   );
 };
 
-export default CardGit
+export default CardGit;
